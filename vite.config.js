05348a2f68937d919374e896abdf4081d4802e3e
Rashidprojects <rashidproject@gmail.com>
2024-09-30 08:01:02 +0530

    sept 30 code

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,33 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-  plugins: [react()],
   build: {
     lib: {
-      // Entry point for the component library
-      entry: 'src/index.ts',  // Ensure this points to your library's entry file
-      name: 'ReactDesignSystem',  // Name of the library
-      fileName: (format) => `react-design-system.${format}.js`,  // Output file name format
-      formats: ['es', 'umd'],  // Output formats: ES modules and UMD
+      entry: resolve(__dirname, './lib/index.ts'),
+      name: 'ReactDesignSystem',
+      fileName: (format) => `react-design-system.${format}.js`,
+      formats: ['es', 'umd'],
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],  // Exclude react and react-dom from the build
+      external: ['react', 'react-dom'],
       output: {
         globals: {
-          react: 'React',  // Global variable name for React
-          'react-dom': 'ReactDOM',  // Global variable name for ReactDOM
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  plugins: [react(), dts()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
     },
   },
 });
